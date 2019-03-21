b6eb586c891ab576c4ebd2247973a997d48701d9
devyboy <dev@udel.edu>
2019-03-21 14:21:08 -0400

    can now hash text

--- nhac.nim
+++ nhac.nim
@@ -1,14 +1,59 @@
-import os, strutils, httpclient, terminal, sequtils
+import os, strutils, httpclient, terminal, sequtils, md5, std/sha1
 
 # Declaring stuff
 var filename : string
-var hash : string
 var hashType: string
 var client = newHttpClient()
-var result : string
 const hashTypes = @["md5", "sha1", "sha256", "sha384", "sha512"]
 
-echo """    
+proc findHash(text, hashType: string) =
+    let response = client.getContent("http://hashtoolkit.com/generate-hash/?text=" & text)
+    if not response.contains(hashType):
+        quit("Error: Hash type not supported\n")
+    else:
+        # I can't use regex so enjoy this terrible string manipulation to find the end quote of the result...
+        let begindexButNotReally = find(response, toLowerAscii(hashType))
+        let realBegindex = begindexButNotReally + find(response[begindexButNotReally .. response.len() - 1], "<span>") + 6
+        let endex = realBegindex + find(response[realBegindex .. response.len() - 1], "</span>")
+        let result = response[realBegindex .. endex - 1]
+
+        echo "Hash Result: " & result & "\n"
+
+proc decode(hash: string) =
+    case hash.len()
+    of 32: hashType = "md5"
+    of 40: hashType = "sha1"
+    of 64: hashType = "sha256"
+    of 96: hashType = "sha384"
+    of 128: hashType = "sha512"
+    else: quit("Error: Hash type not supported\n")
+
+    echo "Hash Type: " & hashType 
+
+    let response = client.getContent("http://hashtoolkit.com/reverse-hash/?hash=" & hash)
+    if response.contains("No hashes found for"):
+        echo "Error: Result not found\n"
+    else:
+        let begindex = find(response, "text=") + 5
+        # More terrible string manipulation, but hey it works
+        let endex = begindex + find(response[begindex .. response.len() - 1], "\"")
+        let result = response[begindex .. endex - 1]
+
+        echo "Result: " & result & "\n"
+
+proc encode(text, hashType: string) =
+    let hashTypeL = toLowerAscii(hashType)
+    if any(hashTypes, proc (x: string): bool = return x == hashTypeL):
+        case hashTypeL
+        of "md5": echo "Result: " & $toMD5(text) & "\n"
+        of "sha1": echo "Result: " & $secureHash(text) & "\n"
+        else: findHash(text, hashTypeL)
+    else:
+        quit("Error: Hash function not recognized\n")
+
+proc main() =
+    echo """  
+      
             oooo                            
             `888                            
 ooo. .oo.    888 .oo.    .oooo.    .ooooo.  
@@ -18,52 +63,42 @@ ooo. .oo.    888 .oo.    .oooo.    .ooooo.
 o888o o888o o888o o888o `Y888""8o `Y8bod8P' 
         
 Nim Hasher and Cracker
-"""
+___________________________________________
+    """
 
-# Incorrect usage/help text
-if paramCount() < 1:
-    quit("Usage for raw hash: ./hash <hash>\nUsage for hash in file: ./hash -f <path to file>")
-# If they used the file flag...
-if paramStr(1) == "-f":
-    # Try to read the file
-    try:
-        filename = paramStr(2)
-    # Error if they used the file flag without actually giving one
-    except IndexError:
-        quit("Please enter a file to read from")  
-    # Set the hash equal to the file contents and strip the whitespace
-    try:
-        hash = readFile(filename).strip()
-    except IOError:
-        quit("Could not open file")
-elif paramStr(1) == "-h":
-    if any(hashTypes, proc (x: string): bool = return x == paramStr(2)):
-        
+    # Incorrect usage/help text
+    if paramCount() < 1:
+        quit("To decode raw hash: ./nhac -d <hash>\nTo encode raw text: ./nhac -e <text>\nTo decode hash in file: ./nhac -df <path to file>\nTo encode text in file: ./nhac -ef <path to file>\n\nSupported Algorithms: md5, sha1, sha256, sha384, sha512\n")
+    if paramStr(1) == "-e":
+        if paramCount() < 3:
+            quit("Error: Enter some text and a hash function\n")
+        else:
+            encode(paramStr(2), paramStr(3))
+    elif paramStr(1) == "-ef":
+        if paramCount() < 3:
+            quit("Error: Enter a file and a hash function\n")
+        try:
+            encode(readFile(paramStr(2)).strip(), paramStr(3))
+        except IOError:
+            quit("Error: Could not open file\n")
+    elif paramStr(1) == "-d":
+        try:
+            decode(paramStr(2))
+        except IndexError:
+            quit("Error: Enter a hash to crack\n")
+    elif paramStr(1) == "-df":
+        # Try to read the file
+        try:
+            filename = paramStr(2)
+        # Error if they used the file flag without actually giving one
+        except IndexError:
+            quit("Error: Enter a file to read from\n")  
+        # Set the hash equal to the file contents and strip the whitespace
+        try:
+            decode(readFile(filename).strip())
+        except IOError:
+            quit("Error: Could not open file\n")
     else:
-        quit("This hash type is not supported")
-else:
-    # If no file or hash flag, just take the 2nd arg as the hash
-    hash = paramStr(1)
-# Determine what kind of hash it is based on the length
-case hash.len()
-of 32: hashType = "md5"
-of 40: hashType = "sha1"
-of 64: hashType = "sha256"
-of 96: hashType = "sha384"
-of 128: hashType = "sha512"
-else: quit("This hash type is not supported")
+        quit("To decode raw hash: ./nhac -d <hash>\nTo encode raw text: ./nhac -e <text>\nTo decode hash in file: ./nhac -df <path to file>\nTo encode text in file: ./nhac -ef <path to file>\n\nSupported Algorithms: md5, sha1, sha256, sha384, sha512\n")
 
-echo "Hash Type: " & hashType
-
-let response = client.getContent("http://hashtoolkit.com/reverse-hash/?hash=" & hash)
-if response.contains("No hashes found for"):
-    echo "Result not found."
-else:
-    # Find where in the HTML response is the result (probably a better way to do this)
-    let begindex = find(response, "text=") + 5
-    # I can't use regex so enjoy this terrible string manipulation to find the end quote of the result...
-    let endex = begindex + find(response[begindex .. response.len() - 1], "\"")
-    result = response[begindex .. endex - 1]
-
-    echo "Hash Found!"
-    echo "Result: " & result & "\n"
+main()
